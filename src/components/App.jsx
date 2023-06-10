b105f0fc4563b694222b355829eeeb90a5d7d131
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-06-10 18:01:07 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,6 @@ export const App = () => {
     { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
     { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
   ]);
-  const [name, setName] = useState('');
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
